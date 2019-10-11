001dc150d67a7ad30476e4fa8daa748e543d0a56
Jnmendza <jonatmendoza@gmail.com>
2019-10-11 09:32:13 -0700

    actions and reducers are up

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from './reducers/reducer'
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
